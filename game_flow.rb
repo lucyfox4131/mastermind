47133206a4bcfb6e6583300d297467fff07473a3
Lucy Fox <lucyreneefox@gmail.com>
2016-03-27 21:50:00 -0600

    working length checker

--- game_flow.rb
+++ game_flow.rb
@@ -3,45 +3,49 @@ require_relative "generate"
 class Game
 
   def initialize
-    @guess = ""
+    @guess = []
     @new_sequence
     @random_sequence
 
-    self.class.generate_sequence
+    generate_sequence
   end
 
-  def self.generate_sequence
+  def generate_sequence
     @new_sequence = Generate.new
     @random_sequence = @new_sequence.random_sequence
     p "This is the #{@random_sequence}"
     get_guess
   end
 
-  def self.get_guess
+  def get_guess
     print "I have generated a beginner sequence with four elements of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit to quit the game at any time. Please enter a guess >"
     @guess = gets.chomp.downcase.chars
     if @guess == 'q'
       exit
     else
-    check_guess_length
+    invalid_length_reguess
+    puts @guess
     end
   end
 
-  def self.check_guess_length
+  def invalid_length_reguess
     if @guess.length < 4
-      until @guess.length == 4 #|| @guess == 'q'
-      puts "That guess is to short. Your guess must be 4 characters long. Please try again. >"
-      @guess = gets.chomp.downcase.chars
-      #if @guess == 'q' then exit end
-      end
+      puts "That guess is too short."
+      wrong_length_new_guess
     elsif @guess.length > 4
-      until @guess.length == 4
-      puts "That guess is to long. Your guess must be 4 characters long. Please try again. >"
-      @guess = gets.chomp.downcase.chars
-      end
+      puts "That guess is too long."
+      wrong_length_new_guess
     end
     puts "Your guess is #{@guess}"
     # check_correct_positions(@guess, @sequence, @beginner)
+  end
+
+  def wrong_length_new_guess
+    until @guess.length == 4 or @guess == ['q']
+      puts "Your guess must be 4 characters long. Please try again. >"
+      @guess = gets.chomp.downcase.chars
+      if @guess == ['q'] then exit end
+    end
   end
 
   # def self.check_correct_positions(array1, array2, difficulty)
