2650ab9ee4d51a51a835713dbc2407b6c1ad86c9
Lucy Fox <lucyreneefox@gmail.com>
2016-03-27 23:03:20 -0600

    refactor game_flow

--- game_flow.rb
+++ game_flow.rb
@@ -4,40 +4,51 @@ class Game
 
   def initialize
     @guess = []
-    @new_sequence
     @random_sequence
+    @current_correct_positions
 
     generate_sequence
   end
 
   def generate_sequence
-    @new_sequence = Generate.new
-    @random_sequence = @new_sequence.random_sequence
+    new_sequence = Generate.new
+    @random_sequence = new_sequence.random_sequence
     p "This is the #{@random_sequence}"
-    get_guess
+    puts "I have generated a beginner sequence with four elements of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit to quit the game at any time."
+    start_game
   end
 
-  def get_guess
-    print "I have generated a beginner sequence with four elements of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit to quit the game at any time. Please enter a guess >"
-    @guess = gets.chomp.downcase.chars
-    if @guess == 'q'
-      exit
-    else
-    invalid_length_reguess
-    puts @guess
+  def start_game
+    # if valid_guess?
+    #  var allCorrect = check_positions? && check colors?
+    #  if allCorrect you win
+    #  else continue and guess again
+    # reguess
+    loop do
+      print "Please enter a guess? >"
+      @guess = gets.chomp.downcase.chars
+      if @guess == ['q']
+        exit
+      elsif validate_guess?
+        puts "VALID LENGTH!"
+      else
+        #wrong_length_new_guess
+        puts "INVALID LENGTH"
+      end
     end
   end
 
-  def invalid_length_reguess
+  def validate_guess?
+    valid_length = false
     if @guess.length < 4
       puts "That guess is too short."
-      wrong_length_new_guess
     elsif @guess.length > 4
       puts "That guess is too long."
-      wrong_length_new_guess
+    else
+      valid_length = true
     end
     puts "Your guess is #{@guess}"
-    # check_correct_positions(@guess, @sequence, @beginner)
+    return valid_length
   end
 
   def wrong_length_new_guess
@@ -48,15 +59,15 @@ class Game
     end
   end
 
-  # def self.check_correct_positions(array1, array2, difficulty)
-  #   difficulty.each do |index|
-  #     is_true =  array1.values_at(index) == array2.values_at(index)
-  #      if is_true == true
-  #        @counter += 1
-  #      end
-  #    end
-  #   p "There are #{counter} colors in the correct positions."
-  #   end
+  def check_correct_positions
+    4.each do |index|
+      is_true =  @guess.values_at(index) == @random_sequence.values_at(index)
+       if is_true == true
+         @current_correct_positions += 1
+       end
+     end
+    p "There are #{@current_correct_positions} colors in the correct positions."
+    end
 
 
 
